eba4e1c51e10b03e8a2770a553c4b1e915db9670
MatviySuk <enwilco@gmail.com>
2023-05-08 14:37:07 +0300

    init client app

---
+++ WarehouseClient/WarehouseClient/WarehouseClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarehouseClientApp.swift
+//  WarehouseClient
+//
+//  Created by Matviy Suk on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WarehouseClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
